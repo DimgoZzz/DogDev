1d38c7d44bc334f4fdbb85e047b9c8f5dc8aebe0
Dimgo <dmgolovinzzz@gmail.com>
2022-02-21 16:42:20 +0300

    proj: add DogFW submodule
    proj: corrected folders

--- DogAppWin/Source/WinMain.cpp
+++ DogAppWin/Source/WinMain.cpp
@@ -1,8 +1,9 @@
 #include "DogFW/External/Windows/DogWindows.h"
+#include "DogFW/DogBasicTypes.h"
+
+using namespace DogFW;
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
 {
-	
-
 	return 0;
 }
